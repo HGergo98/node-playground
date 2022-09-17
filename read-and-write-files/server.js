7a058730e00514e56e4450bff197ff7055ce9946
HGergo <gergo.hegyi@calv.us>
2022-09-17 10:10:27 +0200

    feat: writeFile, append to file and rename file

--- read-and-write-files/server.js
+++ read-and-write-files/server.js
@@ -1,12 +1,28 @@
 const fs = require('fs');
 const path = require('path');
 
+
 fs.readFile(path.join(__dirname, 'files', 'starter.txt') , 'utf8', (err, data) => {
   if (err) throw err;
   console.log(data);
 })
 
-console.log('hello...')
+fs.writeFile(path.join(__dirname, 'files', 'reply.txt'), 'Nice to meet you.', (err) => {
+  if (err) throw err;
+  console.log('Write completed.');
+
+  fs.appendFile(path.join(__dirname, 'files', 'reply.txt'), '\n\nYes it is.', (err) => {
+    if (err) throw err;
+    console.log('Append completed.');
+
+    fs.rename(path.join(__dirname, 'files', 'reply.txt'), path.join(__dirname, 'files', 'new-reply.txt'), (err) => {
+      if (err) throw err;
+      console.log('Rename completed.');
+    })
+
+  })
+
+})
 
 // exit on uncaight errors
 process.on('uncaughtException', err => {
