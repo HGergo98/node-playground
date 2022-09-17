871567e5afa02124780ced16a21e44a704fd92e5
HGergo <gergo.hegyi@calv.us>
2022-09-17 10:17:53 +0200

    feat: replace cb w async function using fsPromises

--- read-and-write-files/server.js
+++ read-and-write-files/server.js
@@ -1,28 +1,25 @@
 const fs = require('fs');
+const fsPromises = require('fs').promises;
 const path = require('path');
 
+const fileOps = async () => {
+  try {
+    const data = await fsPromises.readFile(path.join(__dirname, 'files', 'starter.txt'), 'utf8');
+    console.log(data);
+    await fsPromises.unlink(path.join(__dirname, 'files', 'starter.txt'), data);
 
-fs.readFile(path.join(__dirname, 'files', 'starter.txt') , 'utf8', (err, data) => {
-  if (err) throw err;
-  console.log(data);
-})
+    await fsPromises.writeFile(path.join(__dirname, 'files', 'promiseWrite.txt'), data);
+    await fsPromises.appendFile(path.join(__dirname, 'files', 'promiseWrite.txt'), '\n\nNice to meet you.');
+    await fsPromises.rename(path.join(__dirname, 'files', 'promiseWrite.txt'),path.join(__dirname, 'files', 'promiseComplete.txt'));
 
-fs.writeFile(path.join(__dirname, 'files', 'reply.txt'), 'Nice to meet you.', (err) => {
-  if (err) throw err;
-  console.log('Write completed.');
+    const newData = await fsPromises.readFile(path.join(__dirname, 'files', 'promiseComplete.txt'), 'utf8');
+    console.log(newData)
+  } catch (err) {
+    console.error(err)
+  }
+}
 
-  fs.appendFile(path.join(__dirname, 'files', 'reply.txt'), '\n\nYes it is.', (err) => {
-    if (err) throw err;
-    console.log('Append completed.');
-
-    fs.rename(path.join(__dirname, 'files', 'reply.txt'), path.join(__dirname, 'files', 'new-reply.txt'), (err) => {
-      if (err) throw err;
-      console.log('Rename completed.');
-    })
-
-  })
-
-})
+fileOps();
 
 // exit on uncaight errors
 process.on('uncaughtException', err => {
