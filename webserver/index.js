43c162398ac56e9ad4361581e831b994d53be22e
HGergo <gergo.hegyi@calv.us>
2022-09-19 20:43:08 +0200

    feat: createServer nad handling contentType

--- webserver/index.js
+++ webserver/index.js
@@ -1,11 +1,50 @@
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+const fsPromises = require('fs').promises;
+
 const logEvents = require('./logEvents');
-
 const EventEmitter = require('events');
-
-class MyEmitter extends EventEmitter {};
-
+class Emitter extends EventEmitter {};
 //initalize object
-const myEmitter = new MyEmitter();
+const myEmitter = new Emitter();
 
-//add listener for log event
-myEmitter.on('log', (msg) => logEvents(msg));
+const PORT = process.env.PORT || 3500;
+
+const server = http.createServer((req,res) => {
+  console.log(req.url, req.method);
+
+  const extension = path.extname(req.url);
+
+  let contentType;
+  switch (extension) {
+    case '.css':
+      contentType = 'text/css';
+      break;
+    case '.js':
+      contentType = 'text/javascript';
+      break;
+    case '.json':
+      contentType = 'application/json';
+      break;
+    case '.jpg':
+      contentType = 'text/jpg';
+      break;
+    case '.png':
+      contentType = 'text/png';
+      break;
+    case '.txt':
+      contentType = 'text/plain';
+      break;
+  
+    default:
+      contentType = 'text/html';
+      break;
+  }
+
+})
+
+server.listen(PORT, () => console.log(`Server running on port. ${PORT}`));
+
+// //add listener for log event
+// myEmitter.on('log', (msg) => logEvents(msg));
